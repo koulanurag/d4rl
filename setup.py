2aac69bfe3d87f706a30bdfd5cfb150a931e4f0d
Anurag Koul <koulanurag@gmail.com>
2022-01-03 16:49:07 -0800

    fix: specifing dm_control version

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ setup(
                       'h5py',
                       'termcolor',  # adept_envs dependency
                       'click',  # adept_envs dependency
-                      'dm_control' if 'macOS' in platform() else
-                      'dm_control @ git+git://github.com/deepmind/dm_control@master#egg=dm_control',
+                      'dm_control==0.0.364896371' if 'macOS' in platform() else
+                      'dm_control @ git+git://github.com/deepmind/dm_control@2a0865b50af1bc5033866fd08dbd87bb71a2df32#egg=dm_control',
                       'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
     packages=find_packages(),
     package_data={'d4rl': ['locomotion/assets/*',
